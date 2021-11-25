7d628a8085a56aa2033b93acbd283c35dd77d49e
ThunderShout <thalesaugusto312@gmail.com>
2021-11-25 14:23:04 -0300

    mortadelaboy

--- testgame.py
+++ testgame.py
@@ -1,15 +1,13 @@
 import sys, pygame
-from pygame import display
+from pygame import *
 pygame.init()
-pygame.display.set_caption('Mortadelaboy vs Coxinhaman')
+pygame.display.set_caption('Mortadelaboy')
 programIcon = pygame.image.load('mortadelaboy.png')
 pygame.display.set_icon(programIcon)
-
-size = width, height = 1280, 720
-speed = [2,2]
-black = 0,0,0
-
-screen = pygame.display.set_mode(size)
+bg = pygame.image.load('bg.png')
+screen = pygame.display.set_mode((1280,720))
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+    playerImg = pygame.image.load('mortadelaboy.png')
+    screen.blit(bg,(0,0))
